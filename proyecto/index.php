4daa963c84ec53218840e9044f843338185ff221
Luis Dulanto <l.dulantoflores@gmail.com>
2021-11-08 21:39:28 -0500

    commit

---
+++ proyecto/index.php
@@ -0,0 +1,16 @@
+<?php include("template/cabecera.php");?>
+
+        <div class="jumbotron text-center">
+        <h1 class="display-3">Bienvenido al sitio web de la Biblioteca</h1>
+        <p class="lead">Consulta libros </p>
+        <hr class="my-2">
+
+        <img width="400" src="img/banner.jpg" class="img-thumbnail rounded mx-auto d-block"/>
+        <p>Más información</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver almacen de libros</a>
+        </p>
+    </div>
+
+        
+<?php include("template/pie.php");?>
